71accc0f71a87946e33fb57ae2e32d1ff78c50ec
Pierre-François Léon <pierrefrancois.leon@gmail.com>
2022-01-12 14:40:57 -0500

    Corrections

--- Module06_TableHachage/AA_Module06_TableHachage_PreparationCours/AA_Module06_TableHachage_PreparationCours/CoupleClefValeur.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AA_Module06_TableHachage_PreparationCours
-{
-    public class CoupleClefValeur<TypeClef, TypeValeur>
-    {
-        public TypeClef Clef { get; set; }
-        public TypeValeur Valeur { get; set; }
-        public int ValeurHachage { get; set; }
-    }
-}
